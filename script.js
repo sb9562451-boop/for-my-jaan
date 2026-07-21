36fde7f5694354ac2a9b1e7386b45f72dffeacc1
sb9562451-boop <sb9562451@gmail.com>
2026-07-22 00:11:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,39 @@ I will always care about you.
 }
 
 document.getElementById("btn").onclick = showMessage;
+
+const text = "Every friendship has a story... and you are my favorite chapter. 💖";
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 60);
+  }
+}
+
+window.onload = function () {
+  typeWriter();
+
+  document.getElementById("btn").onclick = function () {
+    alert(`💖 Dear Cherry 💖
+
+You are not just my best friend...
+
+You are my happiness,
+my smile,
+my safe place,
+and one of the best people
+I have ever met.
+
+Thank you for always being with me.
+
+No matter what happens,
+I will always care about you.
+
+🌸 You will always be my Cherry. 🍒
+
+❤️ Made with Love by Sobia ❤️`);
+  };
+};
