4f00730c8e3452475aa2a9c7cceb0901cc71d4fb
sb9562451-boop <sb9562451@gmail.com>
2026-07-21 10:31:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector("button").addEventListener("click", function () {
-    alert("💖 Surprise! 💖\n\nDear Cherry,\nYou are the most amazing friend ever! 🌸❤️");
+  alert("💖 Surprise! 💖\n\nDear Cherry,\nYou are the best friend anyone could ever ask for.\nThank you for always making me smile.\nYou are so amazing and I am so lucky to have you.\n❤️ Love you forever! ❤️");
 });
